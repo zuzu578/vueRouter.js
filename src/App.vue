7c0e560033240b16670a121c86e0aaad1d1f84a5
ZUZU <69393030+zuzu578@users.noreply.github.com>
2021-04-27 17:27:55 +0900

    vue.js Routing

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header />
+    <div id="content" class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+.title
+{
+  text-align: center;
+  margin-top:60px;
+}
+.form-control{
+  width:600px;
+  margin:0 auto;
+}
+</style>
---
+++ src/components/views/Home.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="title"> 
+    <h1> welcome {{title2}}!</h1>
+    <input type="text" v-model="input1"/>
+    <!-- onclick 대신 vue 에서는 @click-->
+    
+    <button type="button" @click="getData">Get</button>
+    <button type="button" @click="setData">Set</button>
+
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <!-- list 를 반복적으로 출력할때 -->
+      <option :key="i" v-for="(d,i) in options">{{d.t}} </option>
+
+    </select>
+    <!-- v-if : true 일때 table 을 show 해줌  조건에 따라 element 를 rendering 해줄지 말지를 결정 -->
+    <table class="table table-bordered" v-if="tableShow">
+      <tr :key="i" v-for="(d,i) in options"> 
+        <td>{{d.t}} </td>
+        <td>{{d.t}} </td>
+        <td>{{d.t}} </td>
+        </tr>
+    </table>
+  </div>
+
+    
+    
+    
+</template>
+<script>
+export default {
+  //DataBinding (two-way)
+  data(){
+    return {
+      title:"dev",
+      title2:"developer",
+      input1:"hello Develpoer",
+      options:[
+        {v:"F",t:"Front-end"},
+        {v:"B",t:"Back-end"},
+        {v:"D",t:"DBA"},
+       
+      ],
+      region: "S",
+      tableShow: true,
+    };
+  },
+  //data value 모니터링 => data 를 입력할때마다 data의 변경을 catch 가능 
+  watch:{
+    input1(){
+      console.log(this.input1);
+    },
+    title(){
+      console.log(this.title);
+    }
+  },
+  //function()
+  methods:{
+    getData(){
+      alert(this.input1);
+    },
+    setData(){
+      this.input1 = "초기화";
+    },
+    changeRegion(){
+      alert(this.region);
+    },
+  },
+  //lifeCycle
+  beforeCreate()
+  {
+    console.log("beforeCreate");
+  },
+  beforeMount()
+  {
+    console.log("beforeMount");
+  },
+  mounted()
+  {
+    console.log("mounted");
+  },
+  beforeUpdate()
+  {
+    console.log("beforeUpdate");
+  },
+  updated()
+  {
+    console.log("updated");
+  },
+  beforeDestroy()
+  {
+    console.log("beforeDestroy");
+  },
+  destroyed()
+  {
+    console.log("destroyed");
+  },
+
+};
+</script>
